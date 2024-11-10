33056874343b0ab9e06ddd4c03cfc571bd0e92c0
dinhtanhuy360 <dinhtanhuy360@gmail.com>
2024-11-10 07:04:18 +0700

    env

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config(); // crt s để thay đổi chương trình
+// import mysql
+const mysql = require("mysql2");
+//config connection to datasbase
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = connection;
